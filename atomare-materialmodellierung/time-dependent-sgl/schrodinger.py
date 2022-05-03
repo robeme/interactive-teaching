e752126954419acc5aee07c583cd5feb4dbf6ffe
robeme <robert.meissner@tuhh.de>
2022-05-03 13:49:18 +0200

    first running version

--- atomare-materialmodellierung/time-dependent-sgl/schrodinger.py
+++ atomare-materialmodellierung/time-dependent-sgl/schrodinger.py
@@ -186,7 +186,7 @@ class Schrodinger(object):
         self.dt = dt
         if Nsteps > 0:
             self.psi_mod_x *= self.x_evolve_half
-            for num_iter in xrange(Nsteps - 1):
+            for num_iter in range(Nsteps - 1):
                 self.compute_k_from_x()
                 self.psi_mod_k *= self.k_evolve
                 self.compute_x_from_k()
